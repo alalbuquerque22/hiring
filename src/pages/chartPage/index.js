ff76bf22766fbed728a19cd8d7dec7e835bed5c8
Andre Albuquerque <alalbuquerque22@gmail.com>
2021-01-22 17:52:19 -0300

    requisicao api e montagem da tabela dinamica

--- src/pages/chartPage/index.js
+++ src/pages/chartPage/index.js
@@ -1,51 +1,47 @@
 import axios from 'axios'
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from 'react'
 import { Line} from 'react-chartjs-2'
 // import Api from '../../services/api'
 import './index.css'
 function ChartApp() {
-
- 
   
     const [index,setIndex] = useState([]);
     const [value,setValue] = useState([]);
     const [dailyValue,setDailyValue] = useState()
     const [optionsState,setOptionsState] = useState('IBM')
-    
-   
+    const [pricesTime,setPricesTime] = useState([])
+    const [secondIndex,setSecondIndex] = useState([]);
+    const [secondValue,setSecondValue] = useState([]);
+    const [secondDailyValue,setSecondDailyValue] = useState()
+    const [secondOptionsState,setSecondOptionsState] = useState('FB')
+    const [secondPricesTime,setSecondPricesTime] = useState([])
+
 
     useEffect(()=>{ 
-      const getStock = async ()=>{
+      const getStock = async () => {
+
         const api_key ='process.env.REACT_APP_ALPHA_KEY';
-        
         const urlQuery =`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${optionsState}&apikey=${api_key}`
         const valueX = []
         const valueY= []
+        const pricesTimeAux = []
     
-    
-        let res = await axios.get(urlQuery)
+        let res = await axios.get(urlQuery) 
+
+        for (const key in res.data['Time Series (Daily)']) {
+         
+          valueX.push(key)
+          valueY.push(res.data['Time Series (Daily)'][key]['4. close'])
+          pricesTimeAux.push({"preco":res.data['Time Series (Daily)'][key]['4. close'], "data":key})
         
-      
-          
-    
-          for (const key in res.data['Time Series (Daily)']) {
-       
-            valueX.push(key);
-            valueY.push(res.data['Time Series (Daily)'][key]['4. close']);
-           
-            
-          }
-          
-          
-          const firstElement = valueY.shift();
+        }       
+
+        const firstElement = valueY.shift()
         
-           
-    
-          setDailyValue(firstElement)
-          setIndex(valueX.reverse());
-          setValue(valueY.reverse())
-    
-          
+        setPricesTime(pricesTimeAux)
+        setDailyValue(firstElement)
+        setIndex(valueX.reverse())
+        setValue(valueY.reverse())
     
       }
       
@@ -54,58 +50,47 @@ function ChartApp() {
     
       // console.log(index,value);
     },[optionsState])
-    
 
-
-    const [secondIndex,setSecondIndex] = useState([]);
-    const [secondValue,setSecondValue] = useState([]);
-    const [secondDailyValue,setSecondDailyValue] = useState()
-    const [secondOptionsState,setSecondOptionsState] = useState('IBM')
    
-
     useEffect(()=>{ 
       const getComparation = async ()=>{
-        const api_key ='process.env.REACT_APP_ALPHA_KEY';
-        
+        const api_key ='process.env.REACT_APP_ALPHA_KEY';  
         const urlQuery =`https://www.alphavantage.co/query?function=TIME_SERIES_DAILY_ADJUSTED&symbol=${secondOptionsState}&apikey=${api_key}`
         const valueX = []
         const valueY= []
-    
+        const secondPricesTimeAux = []
     
         let res = await axios.get(urlQuery)
         
+        for (const key in res.data['Time Series (Daily)']) {
       
+          valueX.push(key);
+          valueY.push(res.data['Time Series (Daily)'][key]['4. close']);
+          secondPricesTimeAux.push({"preco":res.data['Time Series (Daily)'][key]['4. close'], "data":key})
           
-    
-          for (const key in res.data['Time Series (Daily)']) {
-       
-            valueX.push(key);
-            valueY.push(res.data['Time Series (Daily)'][key]['4. close']);
-         
-            
-          }
-          
-          
-          const firstElement = valueY.shift();
+        }
         
-    
-    
-          setSecondDailyValue(firstElement)
-          setSecondIndex(valueX.reverse());
-          setSecondValue(valueY.reverse())
-    
-          
-    
+        const firstElement = valueY.shift();
+      
+        setSecondPricesTime(secondPricesTimeAux)
+        setSecondDailyValue(firstElement)
+        setSecondIndex(valueX.reverse());
+        setSecondValue(valueY.reverse())
+      
       }
       
       getComparation()
-      // console.log(index,value);
+   
     },[secondOptionsState])
   
     const [chartData,setChartData] = useState({})
-    
-    const chart = async() =>{
-       await setChartData({
+    useEffect(()=>{
+      chart()
+  
+    },[index,value])
+  
+    const chart = ()=>{
+        setChartData({
           labels:index,
         
           datasets:[
@@ -113,9 +98,9 @@ function ChartApp() {
               label:'Historico de fechamento por data',
               data:value,
               fill:false,
-              borderColor:'rgba(192,168,10,1)',
+              borderColor:'rgba(23, 0, 241,1)',
               borderWidth:0.5,
-              backgroundColor:'rgba(192,168,10,0.2)'
+              backgroundColor:'rgba(23, 0, 241, 0.2)'
             
             }
           ],
@@ -139,11 +124,7 @@ function ChartApp() {
       )
     }
   
-    useEffect(()=>{
-      chart()
-  
-    },[index,value])
-  
+ 
     const handleChange = (event) => {
       setOptionsState(event.target.value);
     };
@@ -152,36 +133,7 @@ function ChartApp() {
       setSecondOptionsState(event.target.value);
     };
 
-    function handleTimestamp (){
-
-        const linhas = index.map(()=>{
-          return(
-        
-            
-            <tr>
-                <td>{index}</td>
-                <td>U$ {value}</td>
-                <td>U$ {secondValue}</td>
-                <td>{ ((value / secondValue -1 )* 100).toFixed(2)}%</td>
-            </tr>
-            
-
-          )
-
-      })
-          
-      
-      
-
-    }
-  
-
-
-
-   
-
-  
-     
+    
     return (
         <div className="main">
         
@@ -189,8 +141,9 @@ function ChartApp() {
                       <div className="container">
                           <h1 >Histórico</h1>
 
-                        <div className="selection">
+                        <div className="selectionBoxItem">
                           <h2>Histórico da equity: {optionsState} </h2>
+                              <div className="selection">
                               <select 
                               onChange={handleChange}
                               value={optionsState}>
@@ -200,6 +153,7 @@ function ChartApp() {
                                 <option value="WORK">SLACK TECNOLOGIES</option>
                                 <option value="TSLA">TESLA</option>
                               </select>
+                        </div>
                         </div>
 
 
@@ -217,9 +171,10 @@ function ChartApp() {
                           
 
                         <div className="selectionBox">
-                            <div className="selection">
-                              <h2>Equity de : {optionsState} </h2>
-                              <h2> Valor de hoje: U$ {dailyValue}</h2>
+                            <div className="selectionBoxItem">
+                              <h2 style={{fontSize:23}}>Equity de : {optionsState} </h2>
+                              <h2 style={{fontSize:23}}> Valor de hoje: U$ {dailyValue}</h2>
+                              <div className="selection">
                                   <select 
                                   onChange={handleChange}
                                   value={optionsState}>
@@ -229,13 +184,13 @@ function ChartApp() {
                                     <option value="WORK">SLACK TECNOLOGIES</option>
                                     <option value="TSLA">TESLA</option>
                                   </select>
-                            </div>
+                            </div></div>
 
 
-                            <div className="selection">
-                              <h2>Equity de : {secondOptionsState} </h2>
-                              <h2> Valor de hoje: U$ {secondDailyValue}</h2>
-
+                            <div className="selectionBoxItem">
+                              <h2 style={{fontSize:23}}>Equity de : {secondOptionsState} </h2>
+                              <h2 style={{fontSize:23}}> Valor de hoje: U$ {secondDailyValue}</h2>
+                                  <div className="selection">
                                   <select 
                                   onChange={handleSecondChange}
                                   value={secondOptionsState}>
@@ -246,32 +201,51 @@ function ChartApp() {
                                     <option value="TSLA">TESLA</option>
                                   </select>
                             </div>
-
+                            </div>
                         </div>
-
-                      <h2>Diferança entre preço da Ação {optionsState} e {secondOptionsState} é de U$ {dailyValue - secondDailyValue}</h2>
-                      <h2>  A Variação de porcentagem é: { ((dailyValue / secondDailyValue -1 )* 100).toFixed(2)}%</h2> Formula : (({dailyValue} / {secondDailyValue}) -1) *100 
-
+                      <div className="text">
+                        <h2>Legendas:</h2>
+                        <h2 > * Diferança entre preço da Ação {optionsState} e {secondOptionsState} é de U$ {dailyValue - secondDailyValue}</h2>
+                        <h2 > * A Variação de porcentagem é: { ((dailyValue / secondDailyValue -1 )* 100).toFixed(2)}%</h2>
+                        <h2 > * Formula : (({dailyValue} / {secondDailyValue}) -1) *100 </h2>
+                      </div>
 
                         <div className="tabela">
-                        <table style={{  border: "1px solid black"}}>
+                        <table >
                             <tr>
-                                <th>&nbsp;</th>
+                                <th>Data</th>
                                 <th>{optionsState}</th>
+                             
+                              
+                            </tr>
+                            {pricesTime && pricesTime.map((item,index)=> {
+                              return(
+                                  <tr key={index}>
+                                    <td>{item.data}</td>
+                                    <td>U$ {item.preco}</td> 
+                                  </tr>
+                              );
+                             })}
+                        </table>
+                        <table >
+                            <tr>
                                 <th>{secondOptionsState}</th>
                                 <th>Variação de porcentagem</th>
+                                <th>Diferença entre valores</th>
                             </tr>
-                            <tr>
-                                <td>{index.shift()}</td>
-                                <td>U$ {dailyValue}</td>
-                                <td>U$ {secondDailyValue}</td>
-                                <td> { ((dailyValue / secondDailyValue -1 )* 100).toFixed(2)}%</td>
-                            </tr>
-                           
+                            {(secondPricesTime && pricesTime) && secondPricesTime.map((item,index)=> {
+                              return(
+                                  <tr key={index}>
+                                    <td>U$ {item.preco}</td>{/* data */}
+                                    
+                                    <td>{((pricesTime[index].preco / item.preco -1)*100).toFixed(2)}%</td> 
+                                    <td>{(pricesTime[index].preco - item.preco).toFixed(2)}</td> 
+                                  </tr>
+                              );
+                             })}
                         </table>
                         </div>
-
-                     </div>
+                      </div>
                   </div>
         
         </div>      
